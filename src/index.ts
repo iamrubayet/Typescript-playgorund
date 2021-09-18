1516d50fd61fd74e99167578f211e662c610c150
rubayet170746 <rafirubayet75@gmail.com>
2021-09-19 04:10:03 +0600

    array and tuple learnt

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,32 @@ age = 30
 
 
 console.log("ID:",id)
+
+
+
+//Array
+
+
+let id:number [] = [1,2,3]
+
+
+//tuple
+
+
+let person : [number,string,boolen] = [1,'brad',true]
+
+
+//tuple array
+
+
+let employee : [number,string][]
+
+employee = [
+   [1,'brad'],
+   [2,'rub'],
+]
+
+
+
+
+
