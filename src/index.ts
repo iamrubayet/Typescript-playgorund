645a2ca85b083563f4c33352bf70f29e070b757b
rubayet170746 <rafirubayet75@gmail.com>
2021-09-19 06:52:11 +0600

    added all

--- src/index.ts
+++ src/index.ts
@@ -172,5 +172,12 @@ const emp = new Employee(3,"shawn",'developer')
 
 
 
+function getArray<T>(items: T[]): T[] {
+  return new Array().concat(items)
+}
 
+let numArray = getArray<number>([1, 2, 3, 4])
+let strArray = getArray<string>(['brad', 'John', 'Jill'])
+
+strArray.push(1) // Throws error
 
